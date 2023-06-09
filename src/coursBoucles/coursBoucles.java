5c1d68400e7677563a54826fdac838de0da3454a
Thomas Fremaux <ts.fremaux@gmail.com>
2023-06-09 11:27:33 +0200

    ajout de l'exo sur les boucles

---
+++ src/coursBoucles/coursBoucles.java
@@ -0,0 +1,16 @@
+package coursBoucles;
+
+public class coursBoucles {
+
+	public static void main(String[] args) {
+
+		// TODO: Créez une boucle "for" qui affiche la phrase "I am having so much fun!"
+		// 5 fois
+
+		for (int i = 0; i < 5; i++) {
+
+			System.out.println("I am having so much fun!");
+		}
+	}
+
+}
